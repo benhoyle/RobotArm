2831a8ae87379a55aa18dda1ea4512518135cabc
benhoyle <benjhoyle@gmail.com>
2013-11-23 13:11:19 +0000

    Working copy

--- RobotArm.py
+++ RobotArm.py
@@ -1,34 +1,42 @@
 # import the USB and Time libraries into Python
-import usb.core, usb.util, time, thread
+import usb.core, usb.util, time, thread, os
 import elementtree.ElementTree as ET
 
-class robot:
+class Robot:
 	def __init__(self):
 		# Allocate the name 'RoboArm' to the USB device
 		self.arm = usb.core.find(idVendor=0x1267, idProduct=0x0000)
-
 		# Check if the arm is detected and warn if not
 		if self.arm is None:
 			raise ValueError("Arm not found")
 		
 		self.commands = [([0,1,0], [0,2,0]), ([64,0,0], [128,0,0]), ([16,0,0], [32,0,0]), ([4,0,0], [8,0,0])]
 		self.duration = 0.5 #Period of time units
+		self.ranges = []
+		self.positions = []
 		
 		#Load position and range values
-		#Read XML file
-		tree = ET.ElementTree(file='config.xml')
+		#Check if config file exists
+		if os.path.exists('config.xml'):
+			#Read XML file
+			self.tree = ET.ElementTree(file='config.xml')
+		else:
+			print("run config") #error - need to run config
 		
 		#Access ranges tags
-		for elem in tree.iter(tag='ranges'):
-			self.ranges.append(elem.text)
-			
-		for elem in tree.iter(tag='positions'):
-			self.positions.append(#tag value)
+		iter_obj = self.tree.getiterator('ranges')
 		
-		#Access grip_state tag
-		for elem in tree.iterfind('gripstate'):
-			self.gripstate = elem.text
+		for elem in iter_obj[0].getchildren():
+			self.ranges.append(elem.text)
+		iter_obj = self.tree.getiterator('positions')
+		
+		for elem in iter_obj[0].getchildren():
+			self.positions.append(elem.text)
 			
+		print self.ranges, self.positions
+		#Access grip_state
+		for elem in self.tree.getiterator('gripstate'):
+			self.gripstate = elem.text
 			
 	def move(self, TimeUnits, AxisFlag, DirectionFlag):
 		#Move arm along axis
@@ -39,11 +47,11 @@ class robot:
 		
 		#Check movement does not go outside range
 		#Current position + timeunits*[in direction -1 or 1]
-		if DirectionFlag = 0:
-			if (self.positions[AxisFlag] + TimeUnits)>self.ranges[AxisFlag]:
+		if DirectionFlag == 0:
+			if (int(self.positions[AxisFlag]) + TimeUnits)>int(self.ranges[AxisFlag]):
 				raise Exception("Movement out of range")
-		if DirectionFlag = 1:
-			if (self.positions[AxisFlag] - TimeUnits)<0:
+		if DirectionFlag == 1:
+			if (int(self.positions[AxisFlag]) - TimeUnits)<0:
 				raise Exception("Movement out of range")
 				
 		# Start the movement
@@ -54,54 +62,65 @@ class robot:
 		self.arm.ctrl_transfer(0x40,6,0x100,0,ArmCmd,1000)
 		
 		#record new position
-		positiontag = "position_"+AxisFlag
-		if DirectionFlag = 0:
-			newposition = self.positions[AxisFlag] + TimeUnits
+		positiontag = "position_"+str(AxisFlag)
+		if DirectionFlag == 0:
+			newposition = int(self.positions[AxisFlag]) + TimeUnits
 		else:
-			newposition = self.positions[AxisFlag] - TimeUnits
-		self.positions[AxisFlag] = newposition
+			newposition = int(self.positions[AxisFlag]) - TimeUnits
+		self.positions[AxisFlag] = str(newposition)
 		
 		#find position tag in config file
-		for elem in tree.iterfind(positiontag):
+		for elem in self.tree.getiterator(positiontag):
 			elem.text = str(newposition)
-		tree.write("config.xml)
-		
+		self.tree.write("config.xml")
+		print self.ranges, self.positions
 		
 	def grip(self, state):
 		#Open or close grip
 		#state = 0 for close, 1 for open
-		if state = 0:
-			if self.gripstate = True:
+		
+		if state == 0:
+			print "Closing Grip..."
+			if self.gripstate == "True":
+				#print "."
+				#Close Grip
+				self.arm.ctrl_transfer(0x40,6,0x100,0,[1,0,0],1000)
+				time.sleep(0.6)
+				ArmCmd=[0,0,0]
+				self.arm.ctrl_transfer(0x40,6,0x100,0,ArmCmd,1000)
+				print "Grip Closed"
+				#record new state
+				for elem in self.tree.getiterator("gripstate"):
+					elem.text = str(False)
+				self.tree.write("config.xml")
+				self.gripstate = str(False)
+			else:
+				print "Grip Already Closed"
+		
+		if state == 1:
+			print "Opening Grip"
+			if self.gripstate == "False":
+				#print "."
 				#Open Grip
 				self.arm.ctrl_transfer(0x40,6,0x100,0,[2,0,0],1000)
 				time.sleep(0.5)
 				ArmCmd=[0,0,0]
 				self.arm.ctrl_transfer(0x40,6,0x100,0,ArmCmd,1000)
-				
+				print "Grip Open"
 				#record new state
-				for elem in tree.iterfind("gripstate"):
-					elem.text = str(False)
-				tree.write("config.xml)
-		
-		if state = 1:
-			if self.gripstate = False:
-				#Close Grip
-				self.arm.ctrl_transfer(0x40,6,0x100,0,[1,0,0],1000)
-				time.sleep(0.5)
-				ArmCmd=[0,0,0]
-				self.arm.ctrl_transfer(0x40,6,0x100,0,ArmCmd,1000)
-				
-				#record new state
-				for elem in tree.iterfind("gripstate"):
+				for elem in self.tree.getiterator("gripstate"):
 					elem.text = str(True)
-				tree.write("config.xml)
+				self.tree.write("config.xml")
+				self.gripstate = str(True)
+			else:
+				print "Grip Already Open"
 		
 	
 	def light(self, state):
 		#Turn light on (1) or off (0)
 		#MoveArm(1,[0,0,1]) #Light On
-		if state = 1:
+		if state == 1:
 			self.arm.ctrl_transfer(0x40,6,0x100,0,[0,0,1],1000)
 		#MoveArm(1,[0,0,0]) #Light Off
-		if state = 0:
+		if state == 0:
 			self.arm.ctrl_transfer(0x40,6,0x100,0,[0,0,0],1000)
